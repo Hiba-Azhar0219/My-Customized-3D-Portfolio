cbe568c2e3fe04d40f4c5515072bb9166ffbad2b
Hiba-Azhar0219 <hiba.officialwork@gmail.com>
2025-07-27 10:42:46 +0500

    File Structure set

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,13 @@
-import menuIcon from '../public/assets/menu.svg'
+import { useState } from 'react'
 
+const NavItems = () => {
+  return <div className='nav-ul'></div>
+}
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen)
+  }
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto'>
@@ -11,9 +18,20 @@ const Navbar = () => {
           >
             Hiba Azhar
           </a>
-          <button>
-            <img src={menuIcon} alt='toggle' className='w-7 h-7' />
+          <button
+            onClick={toggleMenu}
+            className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex'
+            aria-label='toggle menu'
+          >
+            <img
+              src={isOpen ? '/assets/close.svg' : '/assets/menu.svg'}
+              alt='toggle'
+              className='w-7 h-7'
+            />
           </button>
+          <nav className='sm:flex hidden'>
+            <NavItems />
+          </nav>
         </div>
       </div>
     </header>
