16edd04f45ac0e3408e3e16bae82b2a5db855cee
Hiba-Azhar0219 <hiba.officialwork@gmail.com>
2025-08-03 11:03:43 +0500

    ClientReviews Section Completed

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -20,9 +20,9 @@ const Projects = () => {
     })
   }
   return (
-    <section className='c-space my-20 border border-purple-600'>
+    <section className='c-space my-20'>
       <p className='head-text'>My work</p>
-      <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full border border-white'>
+      <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full'>
         <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200'>
           <div className='absolute top-0  right-0'>
             <img
